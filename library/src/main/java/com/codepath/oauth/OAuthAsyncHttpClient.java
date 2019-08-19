f531e52fc28fd99c7f7ef00a71f24d5a1e22c832
Roger Hu <roger.hu@gmail.com>
2019-08-18 22:53:26 -0700

    Support OAuth2 tokens too

--- library/src/main/java/com/codepath/oauth/OAuthAsyncHttpClient.java
+++ library/src/main/java/com/codepath/oauth/OAuthAsyncHttpClient.java
@@ -3,8 +3,16 @@ package com.codepath.oauth;
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.facebook.stetho.okhttp3.StethoInterceptor;
 import com.github.scribejava.core.model.OAuth1AccessToken;
+import com.github.scribejava.core.model.OAuth2AccessToken;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
 import se.akerfeldt.okhttp.signpost.SigningInterceptor;
 
@@ -17,10 +25,30 @@ public class OAuthAsyncHttpClient extends AsyncHttpClient {
     public static OAuthAsyncHttpClient create(String consumerKey, String consumerSecret, OAuth1AccessToken token) {
         OkHttpOAuthConsumer consumer = new OkHttpOAuthConsumer(consumerKey, consumerSecret);
         consumer.setTokenWithSecret(token.getToken(), token.getTokenSecret());
-        OkHttpClient httpClient = new OkHttpClient.Builder().addNetworkInterceptor(new StethoInterceptor()).addInterceptor(new SigningInterceptor(consumer)).build();
+        OkHttpClient httpClient = new OkHttpClient.Builder()
+                .addNetworkInterceptor(new StethoInterceptor())
+                .addInterceptor(new SigningInterceptor(consumer)).build();
 
         OAuthAsyncHttpClient asyncHttpClient = new OAuthAsyncHttpClient(httpClient);
         return asyncHttpClient;
     }
 
+    public static OAuthAsyncHttpClient create(final OAuth2AccessToken token) {
+        final String bearer = String.format("Bearer %s", token.getAccessToken());
+
+        OkHttpClient httpClient = new OkHttpClient.Builder()
+                .addNetworkInterceptor(new StethoInterceptor())
+                .addInterceptor(new Interceptor() {
+                                    @NotNull
+                                    @Override
+                                    public Response intercept(@NotNull Chain chain) throws IOException {
+                                        Request originalRequest = chain.request();
+                                        Request authedRequest = originalRequest.newBuilder().header("Authorization", bearer).build();
+                                        return chain.proceed(authedRequest);
+                                    }
+                                }).build();
+
+        OAuthAsyncHttpClient asyncHttpClient = new OAuthAsyncHttpClient(httpClient);
+        return asyncHttpClient;
+    }
 }
